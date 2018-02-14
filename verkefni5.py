6213c10622a01bec387f7bbe7af3b3e92e22e9a3
óðinn <odinnben2000@gmail.com>
2018-02-14 10:07:07 +0000

    Create verkefni5.py

---
+++ verkefni5.py
@@ -0,0 +1,19 @@
+from bottle import run, route, template, static_file, error
+import requests
+
+
+response = requests.get('http://apis.is/concerts')
+
+data = response.json()
+
+print(data['results'][0]['eventHallName'])
+
+@route('/')
+def index5():
+    return template('index5', data=data)
+
+@route('/static/<filename>')
+def static_server(filename):
+    return static_file(filename, root=('./static_files'))
+
+run()
